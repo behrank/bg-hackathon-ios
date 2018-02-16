f4ebddd3a2f25e4d6e27dfd6f5fb986a57906468
Behran Kankul <behrank@gmail.com>
2018-02-16 22:48:24 +0300

    Initial Commit

---
+++ GetirGotur/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetirGotur
+//
+//  Created by Behran Kankul on 16.02.2018.
+//  Copyright © 2018 KG Teknoloji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
